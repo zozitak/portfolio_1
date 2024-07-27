ab720e877732c8482dfce753f0506463b0f6770e
Takács Zoltán Gábor <zozitak@gmail.com>
2024-07-27 23:18:05 +0200

    fix namings

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import materials, login, users
+from app.api.routes import softwares, materials, login, users
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(materials.router, prefix="/materials", tags=["materials"])
+api_router.include_router(softwares.router, prefix="/softwares", tags=["softwares"])
--- backend/app/api/routes/materials.py
+++ backend/app/api/routes/materials.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter,status,Response
 router = APIRouter()
 
 @router.get("/",status_code=status.HTTP_204_NO_CONTENT)
-def login_access_token():
+def materials():
     return Response(status_code=status.HTTP_204_NO_CONTENT)

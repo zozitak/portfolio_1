910ca013a3e12313afcaa919ee43ffa7dd69975c
Takács Zoltán Gábor <zozitak@gmail.com>
2024-07-28 19:43:33 +0200

    config update

--- backend/app/base/config.py
+++ backend/app/base/config.py
@@ -15,12 +15,24 @@ from pydantic_core import MultiHostUrl
 
 class Settings(BaseSettings):
 
-    DB_TYPE: str = "sqlite"
-    DB_URL: str
-
     def __init__(self):
-        if self.DB_TYPE == "postgresql":
-            DB_URL = self.SQLALCHEMY_DATABASE_URI()
+        if self.DB_TYPE == "sqlite":
+            self.DB_USER = os.getenv("DB_USER","sqlite")
+            self.DB_PORT = int(os.getenv("DB_PORT",5432))
+            self.DB_SERVER = os.getenv("DB_SERVER","db")
+            self.DB_PASSWORD = os.getenv("DB_PASSWORD","")
+            self.DB_NAME = os.getenv("DB_NAME","app")
+            self.DB_URL = f"sqlite:///./test.db" #sqlite uses a file as db
+        elif self.DB_TYPE == "postgresql":
+            self.DB_USER = os.getenv("DB_USER","postgres")
+            self.DB_PORT = int(os.getenv("DB_PORT",5432))
+            self.DB_SERVER = os.getenv("DB_SERVER","db")
+            self.DB_PASSWORD = os.getenv("DB_PASSWORD","")
+            self.DB_NAME = os.getenv("DB_NAME","app")
+            self.DB_URL = self.SQLALCHEMY_DATABASE_URI()
+
+    DB_TYPE: str = "sqlite" 
+    DB_URL: str 
 
     API_STR: str = "/api/v1"
     APP_NAME: str = "SimMat"
@@ -28,30 +40,23 @@ class Settings(BaseSettings):
     DOMAIN: str = "localhost"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
-    if DB_TYPE == "sqlite":
-        DB_USER: str = os.getenv("DB_USER","sqlite")
-        DB_PORT: int = int(os.getenv("DB_PORT",5432))
-        DB_SERVER: str = os.getenv("DB_SERVER","db")
-        DB_PASSWORD: str = os.getenv("DB_PASSWORD","")
-        DB_NAME: str = os.getenv("DB_NAME","app")
-        DB_URL = f"sqlite:///./test.db" #sqlite uses a file as db
-    elif DB_TYPE == "postgresql":
-        DB_USER: str = os.getenv("DB_USER","postgres")
-        DB_PORT: int = int(os.getenv("DB_PORT",5432))
-        DB_SERVER: str = os.getenv("DB_SERVER","db")
-        DB_PASSWORD: str = os.getenv("DB_PASSWORD","")
-        DB_NAME: str = os.getenv("DB_NAME","app")
+    DB_USER: str
+    DB_PORT: int
+    DB_SERVER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_URL: str
 
-        @computed_field  # type: ignore[misc]
-        @property
-        def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-            return MultiHostUrl.build(
-                scheme="postgresql+psycopg",
-                username=self.DB_USER,
-                password=self.DB_PASSWORD,
-                host=self.DB_SERVER,
-                port=self.DB_PORT,
-                path=self.DB_NAME,
-            )
+    @computed_field  # type: ignore[misc]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_SERVER,
+            port=self.DB_PORT,
+            path=self.DB_NAME,
+        )
 
 settings = Settings()  # type: ignore

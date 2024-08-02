ead03ade6edafac9a7ebfb76d42a8f78a5689807
Takács Zoltán Gábor <zozitak@gmail.com>
2024-08-02 05:12:40 +0200

    user utility update 2

--- backend/app/tests/utils/utils.py
+++ backend/app/tests/utils/utils.py
@@ -16,7 +16,7 @@ def random_email() -> str:
 
 def get_superuser_token_headers(client: TestClient) -> dict[str, str]:
     login_data = {
-        "username": settings.FIRST_SUPERUSER,
+        "username": settings.EMAIL_TEST_SUPER_USER,
         "password": settings.FIRST_SUPERUSER_PASSWORD,
     }
     r = client.post(f"{settings.API_STR}/login/access-token", data=login_data)

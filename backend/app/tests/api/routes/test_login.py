5872bd404c0f0744233470800e0f720984c90ffb
Takács Zoltán Gábor <zozitak@gmail.com>
2024-08-02 05:14:45 +0200

    Login Tests Failing

--- backend/app/tests/api/routes/test_login.py
+++ backend/app/tests/api/routes/test_login.py
@@ -1,10 +1,113 @@
+from unittest.mock import patch
+
 from fastapi.testclient import TestClient
+from sqlmodel import Session, select # type: ignore
 
 from app.main import app
 from app.base.config import settings
+from app.base.security import verify_password
+from app.models import User
+from app.utils import generate_password_reset_token
 
 client = TestClient(app)
 
-def test_api_login():
-    response = client.get(settings.API_STR)
-    assert response.status_code == 204
+def test_get_access_token(client: TestClient) -> None:
+    login_data = {
+        "username": settings.EMAIL_TEST_SUPER_USER,
+        "password": settings.FIRST_SUPERUSER_PASSWORD,
+    }
+    r = client.post(f"{settings.API_STR}/login/access-token", data=login_data)
+    tokens = r.json()
+    assert r.status_code == 200
+    assert "access_token" in tokens
+    assert tokens["access_token"]
+
+def test_get_access_token_incorrect_password(client: TestClient) -> None:
+    login_data = {
+        "username": settings.EMAIL_TEST_SUPER_USER,
+        "password": "incorrect",
+    }
+    r = client.post(f"{settings.API_STR}/login/access-token", data=login_data)
+    assert r.status_code == 400
+
+def test_use_access_token(
+    client: TestClient, superuser_token_headers: dict[str, str]
+) -> None:
+    r = client.post(
+        f"{settings.API_STR}/login/test-token",
+        headers=superuser_token_headers,
+    )
+    result = r.json()
+    print(result)
+    assert r.status_code == 200
+    assert "email" in result
+
+def test_recovery_password(
+    client: TestClient, normal_user_token_headers: dict[str, str]
+) -> None:
+    with (
+        patch("app.base.config.settings.SMTP_HOST", "smtp.example.com"),
+        patch("app.base.config.settings.SMTP_USER", "admin@example.com"),
+    ):
+        email = "test@example.com"
+        r = client.post(
+            f"{settings.API_STR}/password-recovery/{email}",
+            headers=normal_user_token_headers,
+        )
+        assert r.status_code == 200
+        assert r.json() == {"message": "Password recovery email sent"}
+
+def test_recovery_password_user_not_exits(
+    client: TestClient, normal_user_token_headers: dict[str, str]
+) -> None:
+    email = "jVgQr@example.com"
+    r = client.post(
+        f"{settings.API_STR}/password-recovery/{email}",
+        headers=normal_user_token_headers,
+    )
+    assert r.status_code == 404
+
+def test_reset_password(
+    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    token = generate_password_reset_token(email=settings.EMAIL_TEST_SUPER_USER)
+    data = {"new_password": "changethis", "token": token}
+    r = client.post(
+        f"{settings.API_STR}/reset-password/",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    assert r.status_code == 200
+    assert r.json() == {"message": "Password updated successfully"}
+
+    user_query = select(User).where(User.email == settings.EMAIL_TEST_SUPER_USER)
+    user = db.exec(user_query).first()
+    assert user
+    assert verify_password(data["new_password"], user.hashed_password)
+    
+    #set back old password
+    token = generate_password_reset_token(email=settings.EMAIL_TEST_SUPER_USER)
+    data = {"new_password": settings.FIRST_SUPERUSER_PASSWORD, "token": token}
+    r = client.post(
+        f"{settings.API_STR}/reset-password/",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    assert r.status_code == 200
+    assert r.json() == {"message": "Password updated successfully"}
+
+
+def test_reset_password_invalid_token(
+    client: TestClient, superuser_token_headers: dict[str, str]
+) -> None:
+    data = {"new_password": "changethis", "token": "invalid"}
+    r = client.post(
+        f"{settings.API_STR}/reset-password/",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    response = r.json()
+
+    assert "detail" in response
+    assert r.status_code == 400
+    assert response["detail"] == "Invalid token"

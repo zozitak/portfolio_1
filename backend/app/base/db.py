df8431215cbbb662f3c6c3e61543b50ff53a2ee9
Takács Zoltán Gábor <zozitak@gmail.com>
2024-08-02 05:11:02 +0200

    database init and dest function update

--- backend/app/base/db.py
+++ backend/app/base/db.py
@@ -1,8 +1,11 @@
-from sqlmodel import Session, create_engine, select, SQLModel# type: ignore
+from typing import Union
+from sqlmodel import Session, create_engine, select, delete, SQLModel# type: ignore
 
 from app import crud
 from app.base.config import settings
 from app.models import User, UserCreate
+from app.models import Material, MaterialCreate
+from app.models import Simulation_Software, Simulation_SoftwareCreate
 
 engine = create_engine(settings.get_url())
 
@@ -12,16 +15,67 @@ engine = create_engine(settings.get_url())
 
 
 def init_db(session: Session) -> None:
-    #migration
-    SQLModel.metadata.create_all(engine)
-
-    user = session.exec(
+    #create active super user
+    superuser = session.exec(
         select(User).where(User.email == settings.EMAIL_TEST_SUPER_USER)
     ).first()
-    if not user:
+    if not superuser:
         user_in = UserCreate(
             email=settings.EMAIL_TEST_SUPER_USER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.create_user(session=session, user_create=user_in)
+        superuser = crud.create_user(session=session, user_create=user_in)
+
+    #create active normal user
+    normaluser = session.exec(
+        select(User).where(User.email == settings.EMAIL_TEST_USER)
+    ).first()
+    if not normaluser:
+        user_in = UserCreate(
+            email=settings.EMAIL_TEST_USER,
+            password=settings.FIRST_USER_PASSWORD,
+            is_superuser=False,
+        )
+        normaluser = crud.create_user(session=session, user_create=user_in)
+
+    #create test material
+    testmaterial = session.exec(
+        select(Material).where(Material.name == "material")
+    ).first()
+    if not testmaterial:
+        material_in = MaterialCreate(
+            name = "material",
+        )
+        testmaterial = crud.create_material(session=session,material_create=material_in)
+
+    #create test material
+    testsoftware = session.exec(
+        select(Simulation_Software).where(Simulation_Software.name == "software")
+    ).first()
+    if not testsoftware:
+        software_in = Simulation_SoftwareCreate(
+            name = "software",
+        )
+        testsoftware = crud.create_simulation_software(session=session,simulation_software_create=software_in)
+
+
+def dest_db(session: Session):
+    #search
+    statement = select(Material)
+    results = session.exec(statement)
+    for res in results:
+        session.delete(res)
+        session.commit()
+
+    statement = select(Simulation_Software)
+    results = session.exec(statement)
+    for res in results:
+        session.delete(res)
+        session.commit()
+
+    statement = select(User)
+    results = session.exec(statement)
+    for res in results:
+        session.delete(res)
+        session.commit()
--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -5,20 +5,34 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session, delete # type: ignore
 
 from app.base.config import settings
-from app.base.db import engine, init_db
+from app.base.db import engine, init_db, dest_db
 from app.main import app
 from app.models import Material, Simulation_Software, User
+from app.tests.utils.user import authentication_token_from_email
+from app.tests.utils.utils import get_superuser_token_headers
 
 
 @pytest.fixture(scope="session", autouse=True)
 def db() -> Generator[Session, None, None]:
     with Session(engine) as session:
+        dest_db(session)
         init_db(session)
         yield session
-        # statement = delete(Material)
-        # session.execute(statement)
-        # statement = delete(Simulation_Software)
-        # session.execute(statement)
-        # statement = delete(User)
-        # session.execute(statement)
-        # session.commit()
+        dest_db(session)
+
+@pytest.fixture(scope="module")
+def client() -> Generator[TestClient, None, None]:
+    with TestClient(app) as c:
+        yield c
+
+
+@pytest.fixture(scope="module")
+def superuser_token_headers(client: TestClient) -> dict[str, str]:
+    return get_superuser_token_headers(client)
+
+
+@pytest.fixture(scope="module")
+def normal_user_token_headers(client: TestClient, db: Session) -> dict[str, str]:
+    return authentication_token_from_email(
+        client=client, email=settings.EMAIL_TEST_USER, db=db
+    )
